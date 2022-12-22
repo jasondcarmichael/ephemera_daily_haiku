16f77e5f65c2080f30d0d458f41377ce118e4762
Jason Carmichael <jasonc0308@icloud.com>
2022-12-22 13:46:48 -0600

    detail views urls added and working

--- server/ephemera/serializers.py
+++ server/ephemera/serializers.py
@@ -7,15 +7,24 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         many = True,
         read_only = True
     )
+    user_url = serializers.ModelSerializer.serializer_url_field(
+        view_name = 'user_detail'
+    )
     class Meta:
         model = User
         fields = ('id', 'name', 'email', 'password', 'tagline', 'image_url', 'haikus')
+        fields = ('id', 'user_url', 'name', 'email', 'password', 'tagline', 'image_url', 'haikus')
 
 class HaikuSerializer(serializers.HyperlinkedModelSerializer):
     users = serializers.HyperlinkedRelatedField(
         view_name = 'user_detail',
         read_only = True
     )
+    user_id = serializers.PrimaryKeyRelatedField(
+        queryset = User.objects.all(),
+        source = 'user'
+    )
     class Meta:
         model = Haiku
         fields = ('id', 'title', 'body', 'created_on', 'users')
+        fields = ('id', 'user_id', 'title', 'body', 'created_on', 'users')
