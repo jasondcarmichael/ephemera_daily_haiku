d0a715a1f4bc4f1afcc386049ecd2617f969d5d1
Jason Carmichael <jasonc0308@icloud.com>
2022-12-22 14:30:02 -0600

    switched order of serializers so all haiku data in user object

--- server/ephemera/serializers.py
+++ server/ephemera/serializers.py
@@ -1,20 +1,6 @@
 from rest_framework import serializers
 from .models import User, Haiku
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    haikus = serializers.HyperlinkedRelatedField(
-        view_name = 'haiku_detail',
-        many = True,
-        read_only = True
-    )
-    user_url = serializers.ModelSerializer.serializer_url_field(
-        view_name = 'user_detail'
-    )
-    class Meta:
-        model = User
-        fields = ('id', 'name', 'email', 'password', 'tagline', 'image_url', 'haikus')
-        fields = ('id', 'user_url', 'name', 'email', 'password', 'tagline', 'image_url', 'haikus')
-
 class HaikuSerializer(serializers.HyperlinkedModelSerializer):
     users = serializers.HyperlinkedRelatedField(
         view_name = 'user_detail',
@@ -28,3 +14,42 @@ class HaikuSerializer(serializers.HyperlinkedModelSerializer):
         model = Haiku
         fields = ('id', 'title', 'body', 'created_on', 'users')
         fields = ('id', 'user_id', 'title', 'body', 'created_on', 'users')
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    haikus = HaikuSerializer(
+        many = True,
+        read_only = True
+    )
+    class Meta:
+        model = User
+        fields = ('id', 'name', 'email', 'password', 'tagline', 'image_url', 'haikus')
+        
+
+        
+# class UserSerializer(serializers.HyperlinkedModelSerializer):
+#     haikus = serializers.HyperlinkedRelatedField(
+#         view_name = 'haiku_detail',
+#         many = True,
+#         read_only = True
+#     )
+#     user_url = serializers.ModelSerializer.serializer_url_field(
+#         view_name = 'user_detail'
+#     )
+#     class Meta:
+#         model = User
+#         fields = ('id', 'name', 'email', 'password', 'tagline', 'image_url', 'haikus')
+#         fields = ('id', 'user_url', 'name', 'email', 'password', 'tagline', 'image_url', 'haikus')
+
+# class HaikuSerializer(serializers.HyperlinkedModelSerializer):
+#     users = serializers.HyperlinkedRelatedField(
+#         view_name = 'user_detail',
+#         read_only = True
+#     )
+#     user_id = serializers.PrimaryKeyRelatedField(
+#         queryset = User.objects.all(),
+#         source = 'user'
+#     )
+#     class Meta:
+#         model = Haiku
+#         fields = ('id', 'title', 'body', 'created_on', 'users')
+#         fields = ('id', 'user_id', 'title', 'body', 'created_on', 'users')
