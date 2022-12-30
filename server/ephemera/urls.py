07cad47027eb7374972e2180270442b93e2dac2b
Jason Carmichael <jasonc0308@icloud.com>
2022-12-30 16:54:07 -0600

    simplejwt installed and working

--- server/ephemera/urls.py
+++ server/ephemera/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from rest_framework.routers import DefaultRouter
+
+
 
 urlpatterns = [
     path('', views.UserList.as_view(), name='user_list'),
     path('users/<int:pk>', views.UserDetail.as_view(), name='user_detail'),
     path('haikus/', views.HaikuList.as_view(), name='haiku_list'),
-    path('haikus/<int:pk>', views.HaikuDetail.as_view(), name='haiku_detail')
+    path('haikus/<int:pk>', views.HaikuDetail.as_view(), name='haiku_detail'),
+    path('api/', include('ephemera.api.urls')),
 ]
--- server/ephemera_django/urls.py
+++ server/ephemera_django/urls.py
@@ -17,8 +17,9 @@ from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ephemera.urls')),
-    path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
