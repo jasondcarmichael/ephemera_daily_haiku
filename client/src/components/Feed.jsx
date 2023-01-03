eb904e6e705e0aa2147d40ad6ededa87bf3330df
Jason Carmichael <jasonc0308@icloud.com>
2023-01-03 16:25:57 -0600

    moved daily inspiration to feed

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react'
+import { format } from 'date-fns'
+import { Card } from 'flowbite-react'
 import axios from 'axios'
 
 export default function Feed() {
@@ -24,40 +26,70 @@ export default function Feed() {
     } else {
     
     return (
-    <section className="bg-white dark:bg-gray-900">
-    <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-        <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
-            <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Recent Haikus</h2>
-        </div> 
-        <div className="grid gap-8 lg:grid-cols-2">
-            {haikus.map((haiku) => (
-            <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700" key={haiku.id}>
-                <div className="flex justify-between items-center mb-5 text-gray-500">
-                    <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                        <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
-                        Haiku
-                    </span>
-                    <span className="text-sm">{haiku.created_on}</span>
-                </div>
-                <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">{haiku.title}</a></h2>
-                <p className="mb-5 font-light text-gray-500 dark:text-gray-400" id="haiku-body">{haiku.body}</p>
-                <div className="flex justify-between items-center">
-                    <div className="flex items-center space-x-4">
-                        <img className="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Leos avatar" />
-                        <span className="font-medium dark:text-white">
-                        BY {haiku.user_id} username
-                        </span>
-                    </div>
-                    <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
-                        Read more
-                        <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                    </a>
-                </div>
+
+        <div className="feed-wrapper">
+           
+            <div className="max-w-sm">
+                <Card
+                    imgAlt="Basho painting of waterfall"
+                    imgSrc="https://upload.wikimedia.org/wikipedia/commons/7/74/Basho_Horohoroto.jpg"
+                >
+                        <div className="flex justify-between items-center mb-3 text-gray-500">
+                            <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                                <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
+                                Daily Inspiration
+                            </span>
+                            <span className="text-sm">{format(new Date(), "EEEE, MM/dd/yyyy") }</span>
+                        </div>
+                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    Today's Haiku
+                    </h5>
+                    <p className="font-normal text-gray-700 dark:text-gray-400">
+                        trying to walk straight<br></br>
+                        down a path of logicality<br></br>
+                        while tipsy<br></br>
+                    </p>
+                    <p className="font-normal text-gray-700 dark:text-gray-400">- <em>Meitei 1894-1965</em></p>
+
+                </Card>
+            </div>
+        
             
-            </article> 
-            ))}
-        </div>  
+            <section className="bg-white dark:bg-gray-900">
+            <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+                <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+                    <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Recent Haikus</h2>
+                </div> 
+                <div className="grid gap-8 lg:grid-cols-2">
+                    {haikus.map((haiku) => (
+                    <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700" key={haiku.id}>
+                        <div className="flex justify-between items-center mb-5 text-gray-500">
+                            <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                                <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
+                                Haiku
+                            </span>
+                            <span className="text-sm">{haiku.created_on}</span>
+                        </div>
+                        <h2 className="mb-2 text-1xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">{haiku.title}</a></h2>
+                        <p className="mb-5 font-light text-gray-500 dark:text-gray-400" id="haiku-body">{haiku.body}</p>
+                        <div className="flex justify-between items-center">
+                            <div className="flex items-center space-x-4">
+                                <img className="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Leos avatar" />
+                                <span className="font-medium dark:text-white">
+                                BY {haiku.user_id} username
+                                </span>
+                            </div>
+                            <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
+                                Read more
+                                <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                            </a>
+                        </div>
+                    
+                    </article> 
+                    ))}
+                </div>  
+            </div>
+            </section>
     </div>
-    </section>
     )
 }}
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -41,23 +41,7 @@ export default function Home() {
                     </figure>
                 </div>
             </section>
-
-            </div>
-
-            <div>
-                <h2>Today is {format(new Date(), "EEEE, MM/dd/yyyy") }</h2>
-            </div>
-            <br></br>
-            <div>
-                <p> trying to walk straight<br></br>
-                    down a path of logicality<br></br>
-                    while tipsy<br></br>
-                </p>
-                <br></br>
-                <p>- Meitei 1894-1965</p>
             </div>
         </div>
     )
 }
-
-// text-2xl font-medium text-gray-900 dark:text-white
