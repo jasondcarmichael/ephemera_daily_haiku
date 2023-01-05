4c030b4aedb623b49c2231f557c57ef24c78926c
Jason Carmichael <jasonc0308@icloud.com>
2023-01-05 11:50:49 -0600

    create component forms cleaned up

--- client/src/components/CreatePost.jsx
+++ client/src/components/CreatePost.jsx
@@ -1,13 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import { useState, useContext } from "react";
-import { Label, TextInput, Textarea, Button } from "flowbite-react"
+import { Card, TextInput, Textarea, Button } from "flowbite-react"
+import { format } from 'date-fns'
 import { DataContext } from "../DataContext";
 import Client from "../services/api";
 
 export default function CreatePost() {
 
     let navigate = useNavigate()
-    const {user, setUser, profiles, setProfiles} = useContext(DataContext)
+    const {user, profiles} = useContext(DataContext)
 
     const initialState = {
         createdOn: "",
@@ -36,68 +37,68 @@ export default function CreatePost() {
             .then((response) => {
             console.log(response.status);
             console.log(response.data.token);
+            navigate('/feed')
           });
 
         } catch (error) {
             throw error
-        }
-        
+        } 
     };
 
     
-      return (
-        <div className="create-haiku">
-          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-            <div>
-              <div className="mb-2 block">
-                <Label
-                  htmlFor="title"
-                  value="Title"
-                />
-              </div>
-              <TextInput
-                id="title"
-                type="text"
-                placeholder="Title..."
-                required={true}
-                maxLength="50" onChange={handleChange} value={formState.title}
-              />
-            </div>
-          <div id="textarea">
-            <div className="mb-2 block">
-              <Label
-                htmlFor="body"
-                value="Your Haiku"
-              />
-            </div>
-            <Textarea
-              id="body"
-              placeholder="Write here..."
+  return (
+    <div className="create-haiku-wrapper">
+      <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+          <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Write a Haiku</h2>
+      </div> 
+
+      <div className="max-w-sm">
+            <Card
+                imgAlt="Bamboo"
+                imgSrc="https://www.kinderart.com//wp-content/uploads/japanesebrush1-1024x773.png"
+            >
+                    <div className="flex justify-between items-center mb-3 text-gray-500">
+                        <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                            Daily Inspiration
+                        </span>
+                        <span className="text-primary-800 text-xs font-medium ">{format(new Date(), "EEEE, MM/dd/yyyy") }</span>
+                    </div>
+                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Today's Writing Prompt
+                </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                    Write about today's weather. How does it feel? How is it impacting your world?
+                </p>
+            </Card>
+        </div>
+      <div className="create-haiku-form">
+        <Card>
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+          <div>
+            <TextInput
+              id="title"
+              type="text"
+              placeholder="Title..."
               required={true}
-              rows={3}
-              maxLength="255"
-              onChange={handleChange} value={formState.body}
+              maxLength="50" onChange={handleChange} value={formState.title}
             />
           </div>
-            <Button type="submit">
-              Post
-            </Button>
-          </form>
+        <div id="textarea">
+          <Textarea
+            id="body"
+            placeholder="Your haiku..."
+            required={true}
+            rows={3}
+            maxLength="255"
+            onChange={handleChange} value={formState.body}
+          />
         </div>
-      )
-    }
-
-//     return (
-//       <div>
-//           <h2>Write a Haiku</h2>
-//           <form onSubmit={handleSubmit}>
-//               <p>Title</p>
-//               <input id="title" type="text" placeholder="Title here" maxLength="50" onChange={handleChange} value={formState.title} />
-//               <input id="body" type="text" placeholder="Haiku here" maxLength="255" onChange={handleChange} value={formState.body} />
-              
-//               <input type="submit" />
-//           </form>
-//       </div>
-
-//   )
-// }
+          <Button type="submit">
+            Post
+          </Button>
+        </form>
+        </Card>
+      </div>
+    </div>
+  )
+}
--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -27,8 +27,10 @@ export default function Feed() {
     
     return (
 
-        <div className="feed-wrapper">
-           
+        <div className="feed-container">
+            <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+                <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Feed</h2>
+            </div> 
             <div className="max-w-sm">
                 <Card
                     imgAlt="Basho painting of waterfall"
@@ -36,13 +38,12 @@ export default function Feed() {
                 >
                         <div className="flex justify-between items-center mb-3 text-gray-500">
                             <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                                <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
                                 Daily Inspiration
                             </span>
-                            <span className="text-sm">{format(new Date(), "EEEE, MM/dd/yyyy") }</span>
+                            <span className="text-primary-800 text-xs font-medium ">{format(new Date(), "EEEE, MM/dd/yyyy") }</span>
                         </div>
                     <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    Today's Haiku
+                    Today's Classical Haiku
                     </h5>
                     <p className="font-normal text-gray-700 dark:text-gray-400">
                         trying to walk straight<br></br>
@@ -50,46 +51,40 @@ export default function Feed() {
                         while tipsy<br></br>
                     </p>
                     <p className="font-normal text-gray-700 dark:text-gray-400">- <em>Meitei 1894-1965</em></p>
-
                 </Card>
             </div>
-        
-            
-            <section className="bg-white dark:bg-gray-900">
-            <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-                <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
-                    <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Recent Haikus</h2>
-                </div> 
-                <div className="grid gap-8 lg:grid-cols-2">
-                    {haikus.map((haiku) => (
-                    <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700" key={haiku.id}>
-                        <div className="flex justify-between items-center mb-5 text-gray-500">
-                            <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                                <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
-                                Haiku
-                            </span>
-                            <span className="text-sm">{haiku.created_on}</span>
-                        </div>
-                        <h2 className="mb-2 text-1xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">{haiku.title}</a></h2>
-                        <p className="mb-5 font-light text-gray-500 dark:text-gray-400" id="haiku-body">{haiku.body}</p>
-                        <div className="flex justify-between items-center">
-                            <div className="flex items-center space-x-4">
-                                <img className="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Leos avatar" />
-                                <span className="font-medium dark:text-white">
-                                BY {haiku.user_id} username
+
+            <div className="feed-wrapper">
+                <section className="bg-white dark:bg-gray-900">
+                <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+                    <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+                        <h4 className="mb-4 text-2xl lg:text-3xl tracking-tight font-extrabold text-gray-900 dark:text-white">Recent Posts</h4>
+                    </div> 
+                    <div className="grid gap-8 lg:grid-cols-2">
+                        {haikus.map((haiku) => (
+                        <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700" key={haiku.id}>
+                            <div className="flex justify-between items-center mb-5 text-gray-500">
+                                <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                                    Haiku
                                 </span>
+                                <span className="text-sm">{haiku.created_on}</span>
                             </div>
-                            <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
-                                Read more
-                                <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                            </a>
-                        </div>
-                    
-                    </article> 
-                    ))}
-                </div>  
+                            <h2 className="mb-2 text-1xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">{haiku.title}</a></h2>
+                            <p className="mb-5 font-light text-gray-500 dark:text-gray-400" id="haiku-body">{haiku.body}</p>
+                            <div className="flex justify-between items-center">
+                                <div className="flex items-center space-x-4">
+                                    <img className="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Leos avatar" />
+                                    <span className="font-medium dark:text-white">
+                                    By poet # {haiku.user_id}
+                                    </span>
+                                </div>
+                            </div>
+                        </article> 
+                        ))}
+                    </div>  
+                </div>
+                </section>
             </div>
-            </section>
     </div>
     )
 }}
